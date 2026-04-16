0bee8e620856be14ca4b24cbd6be894c93051af9
shuvo2011 <shuvombstu2011@gmail.com>
2026-04-16 11:33:15 +0600

    chore: add essential packages, global error handler, not-found middleware, cloudinary config, and setup app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,40 @@
 import express, { Application } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./app/lib/auth";
+import { envVars } from "./app/config/env";
+import { IndexRoutes } from "./app/routes";
+import qs from "qs";
+import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { notFound } from "./app/middleware/notFound";
 
 const app: Application = express();
-// Enable URL-encoded form data parsing
-app.use(express.urlencoded({ extended: true }));
+app.set("query parser", (str: string) => qs.parse(str));
+app.set("view engine", "ejs");
+// Better Auth handler
+app.use("/api/auth", toNodeHandler(auth));
 
-// Middleware to parse JSON bodies
+// CORS
+app.use(
+	cors({
+		origin: [envVars.FRONTEND_URL, "http://localhost:3000"],
+		credentials: true,
+		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	}),
+);
+
+// Middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-	res.send("Server is running!");
-});
+// Routes
+app.use("/api/v1", IndexRoutes);
+
+// Error handlers
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,4 +1,6 @@
 import dotenv from "dotenv";
+import AppError from "../errorHelpers/AppError";
+import status from "http-status";
 dotenv.config();
 
 interface EnvConfig {
@@ -8,6 +10,12 @@ interface EnvConfig {
 	BETTER_AUTH_SECRET: string;
 	BETTER_AUTH_URL: string;
 	FRONTEND_URL: string;
+
+	CLOUDINARY: {
+		CLOUDINARY_CLOUD_NAME: string;
+		CLOUDINARY_API_KEY: string;
+		CLOUDINARY_API_SECRET: string;
+	};
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -17,13 +25,19 @@ const loadEnvVariables = (): EnvConfig => {
 		"DATABASE_URL",
 		"BETTER_AUTH_SECRET",
 		"BETTER_AUTH_URL",
-
 		"FRONTEND_URL",
+		"CLOUDINARY_CLOUD_NAME",
+		"CLOUDINARY_API_KEY",
+		"CLOUDINARY_API_SECRET",
 	];
 
 	requireEnvVariable.forEach((variable) => {
 		if (!process.env[variable]) {
-			throw new Error(`Environment variable ${variable} is required but not set in .env file.`);
+			// throw new Error(`Environment variable ${variable} is required but not set in .env file.`);
+			throw new AppError(
+				status.INTERNAL_SERVER_ERROR,
+				`Environment variable ${variable} is required but not set in .env file.`,
+			);
 		}
 	});
 
@@ -33,8 +47,13 @@ const loadEnvVariables = (): EnvConfig => {
 		DATABASE_URL: process.env.DATABASE_URL as string,
 		BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
 		BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
-
 		FRONTEND_URL: process.env.FRONTEND_URL as string,
+
+		CLOUDINARY: {
+			CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+			CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+			CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
+		},
 	};
 };
 
